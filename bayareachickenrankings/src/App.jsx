a0f4c0feca7e9c0de7508bdf50d2f7edc27db232
Corbin <csplatti@gmail.com>
2025-08-24 13:36:51 -0700

    Fixed Tier list box colors and labels

--- bayareachickenrankings/src/App.jsx
+++ bayareachickenrankings/src/App.jsx
@@ -7,20 +7,42 @@ function App() {
 //   const [count, setCount] = useState(0)
 
   return (
+    <>
     <div className="tierlist_container">
-        <div className="S-label label">S</div>
+        <div className="S-label label">
+            <span class="label_text">S</span>
+        </div>
         <div className="S-area"></div>
-        <div className="A-label label">A</div>
+
+        <div className="A-label label">
+            <span class="label_text">A</span>
+        </div>
         <div className="A-area"></div>
-        <div className="B-label label">B</div>
+
+        <div className="B-label label">
+            <span class="label_text">B</span>
+        </div>
         <div className="B-area"></div>
-        <div className="C-label label">C</div>
+
+        <div className="C-label label">
+            <span class="label_text">C</span>
+        </div>
         <div className="C-area"></div>
-        <div className="D-label label">D</div>
+
+        <div className="D-label label">
+            <span class="label_text">D</span>
+        </div>
         <div className="D-area"></div>
-        <div className="F-label label">F</div>
+
+        <div className="F-label label">
+            <span class="label_text">F</span>
+        </div>
         <div className="F-area"></div>
     </div>
+    <div>
+        
+    </div>
+    </>
   )
 }
 
