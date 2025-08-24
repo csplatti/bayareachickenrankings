3c6b5e2c0077ff6a5f1acffb6431ad7ab28c0e6f
Corbin <csplatti@gmail.com>
2025-08-23 19:39:34 -0700

    Initial UI

--- bayareachickenrankings/src/App.jsx
+++ bayareachickenrankings/src/App.jsx
@@ -4,31 +4,23 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+//   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="tierlist_container">
+        <div className="S-label label">S</div>
+        <div className="S-area"></div>
+        <div className="A-label label">A</div>
+        <div className="A-area"></div>
+        <div className="B-label label">B</div>
+        <div className="B-area"></div>
+        <div className="C-label label">C</div>
+        <div className="C-area"></div>
+        <div className="D-label label">D</div>
+        <div className="D-area"></div>
+        <div className="F-label label">F</div>
+        <div className="F-area"></div>
+    </div>
   )
 }
 
